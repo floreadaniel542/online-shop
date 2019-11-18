a1219656ec15ae39cb32d327f03f8798ef2138b0
flore <floreadaniel542@yahoo.com>
2019-11-18 21:32:30 +0200

    Finaly Day 18.11.2019

---
+++ src/main/java/org/fasttrackit/onlineshop/ProductService.java
@@ -0,0 +1,39 @@
+package org.fasttrackit.onlineshop;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(ProductService.class);
+
+    //IoC - Inversion of Control
+    private final ProductRepository productRepository;
+
+    //Dependency injection
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public Product createProduct(SaveProductRequest request) {
+        LOGGER.info("Creating product {} ", request);
+        Product prodcut = new Product();
+        prodcut.setDescription(request.getDescription());
+        prodcut.setName(request.getName());
+        prodcut.setPrice(request.getPrice());
+        prodcut.setQuantity(request.getQuantity());
+        prodcut.setImageUrl(request.getImageUrl());
+
+       return productRepository.save(prodcut);
+    }
+
+}
--- src/main/java/org/fasttrackit/onlineshop/service/OnlineShopApplication.java
+++ src/main/java/org/fasttrackit/onlineshop/service/OnlineShopApplication.java
@@ -1,4 +1,4 @@
-package org.fasttrackit.onlineshop;
+package org.fasttrackit.onlineshop.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
